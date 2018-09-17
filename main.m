53011761742293c95ce60066fc90b78ac632325b
ashu41 <ashutosh.41k@gmail.com>
2018-09-17 23:00:50 +0530

    added main file to call other functions

--- main.m
+++ main.m
@@ -1,4 +1,16 @@
 %% runs all the function calling scripts here
 
-[s_l,s_f] = 
-[vel_alpha,vel_f,err] = follower_control(s_l,s_f,err_old);
+%initialize variables
+err_old = 0;
+key = uicontrol('Style', 'PushButton', ...
+                    'String', 'Break', ...
+                    'Callback', 'delete(gcbf)');
+
+while (ishandle(key))
+% getting robot states from top mounted camera
+    [s_l,s_f] = camera_feedback();
+    
+    [vel_alpha,vel_f,err] = follower_control(s_l,s_f,err_old);
+    err_old = err;
+%     pause(0.5);
+end
